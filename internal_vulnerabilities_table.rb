3e73f4bc697b3b3de686020e7a0c4b059e35265b
Ian Williams <ian@fishermansenemy.com>
2014-04-28 11:10:08 +0100

    Updated internal vulnerabilities table to add hostname

--- internal_vulnerabilities_table.rb
+++ internal_vulnerabilities_table.rb
@@ -3,17 +3,19 @@ require 'nessus'
 
 outfile = File.new("vulnerabilties.csv","w")
 
-Nessus::Parse.new("//Users//ianwilliams//Documents//testdata//nessus_report.nessus") do |scan|
+Nessus::Parse.new("//Users//ianwilliams//Documents//testdata//DudleyMBC//nessus_report_Dudley1.nessus") do |scan|
 	scan.each_host do |host|
 		high = host.high_severity_count
 		medium = host.medium_severity_count
 		low = host.low_severity_count
-		linestart="#{host.ip},"
+		linestart="#{host.ip},#{host.netbios_name},"
 		linestart+="#{host.os.delete"\n"}" if host.os.respond_to?('delete')
 		host.each_event do |event|
 			lineend=",#{event.name.delete','},#{event.severity.in_words.upcase.split(' ')[0]},#{event.cvss_base_score}"
 			if (event.severity.medium? || event.severity.high? || event.severity.critical?)
 				outfile.puts linestart+lineend
+				linestart = ",,"
+				linestart+="#{host.os.delete"\n"}" if host.os.respond_to?('delete')
 			end
 		end
 	end
